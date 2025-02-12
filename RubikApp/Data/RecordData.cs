3eea4e7a3838e8a87ed60641770e3fc5b092f3e7
logandev <logandvllrd@gmail.com>
2025-02-12 22:57:50 +0100

    added time record data structure

---
+++ RubikApp/Data/RecordData.cs
@@ -0,0 +1,16 @@
+﻿namespace RubikApp.Data
+{
+    public struct RecordData
+    {
+        int SolveIndex { get; set; }
+        TimeRecordData SingleTime { get; set; }
+        TimeRecordData AverageOfFive { get; set; }
+        TimeRecordData AverageOfTwelve { get; set; }
+    }
+
+    public struct TimeRecordData
+    {
+        TimeSpan Time { get; set; }
+        bool BestTime { get; set; }
+    }
+}
